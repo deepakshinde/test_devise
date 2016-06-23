af4aee9482da9ba0ee181840f2731c277aecd7e9
Deepak Shinde <deepak@dpktest.lan.coriolis.co.in>
2016-06-23 14:58:54 +0530

    Created sample app for sign_in/sign_up using devise gem

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,12 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+  after_filter :set_csrf_cookie
+
+  def set_csrf_cookie
+    if protect_against_forgery?
+      cookies['XSRF-TOKEN'] = form_authenticity_token
+    end
+  end
+end
---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,5 @@
+class RegistrationsController < Devise::RegistrationsController
+  skip_before_filter :verify_authenticity_token, :only => :create 
+  clear_respond_to  
+  respond_to :json
+end
